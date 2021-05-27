525d62622cd483dc89e4bb53fea87c24a61b2dac
Eric Brandwein <brandweineric@gmail.com>
2021-05-26 22:34:58 -0300

    Simple bot

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python3 programacion-competitiva-bot.py
---
+++ programacion-competitiva-bot.py
@@ -0,0 +1,36 @@
+import os
+from telegram.ext import Updater, CommandHandler
+import logging
+
+
+PORT = int(os.environ.get('PORT', 8443))
+
+TOKEN_ENVVAR = 'TELEGRAM_TOKEN'
+TOKEN = os.getenv(TOKEN_ENVVAR)
+
+def start(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='¡Hola! '
+        'De ahora en más te voy a mandar un problema de Codeforces al azar por día.\n'
+        'Para frenarme, mandá /stop.'
+    )
+
+
+def main():
+    updater = Updater(token=TOKEN)
+    dispatcher = updater.dispatcher
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
+    updater.bot.setWebhook('https://programacion-competitiva-bot.herokuapp.com/' + TOKEN)
+
+
+if __name__ == '__main__':
+    main()
