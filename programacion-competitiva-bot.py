b72bc88e3e3726bb5b7f1d68fb883e67790ba43a
Eric Brandwein <brandweineric@gmail.com>
2021-05-26 23:13:30 -0300

    Add \dame

--- programacion-competitiva-bot.py
+++ programacion-competitiva-bot.py
@@ -1,6 +1,8 @@
 import os
 from telegram.ext import Updater, CommandHandler
 import logging
+import codeforces_api
+import random
 
 
 PORT = int(os.environ.get('PORT', '8443'))
@@ -8,12 +10,22 @@ PORT = int(os.environ.get('PORT', '8443'))
 TOKEN_ENVVAR = 'TELEGRAM_TOKEN'
 TOKEN = os.getenv(TOKEN_ENVVAR)
 
+cf_api = codeforces_api.CodeforcesApi()
+
 def start(update, context):
     context.bot.send_message(
         chat_id=update.effective_chat.id,
         text='¡Hola! '
-        'De ahora en más te voy a mandar un problema de Codeforces al azar por día.\n'
-        'Para frenarme, mandá /stop.'
+        'Para pedir un problema aleatorio de Codeforces, usá el comando \dame.'
+    )
+
+
+def dame(update, context):
+    problems = cf_api.problemset_problems()['problems']
+    problem = random.choice(problems)
+    context.bot.send_message(
+        f'{problem.name}\n'
+        f'https://codeforces.com/problemset/problem/{problem.contest_id}/{problem.index}'
     )
 
 
@@ -26,7 +38,9 @@ def main():
     )
 
     start_handler = CommandHandler('start', start)
+    dame_handler = CommandHandler('dame', dame)
     dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(dame_handler)
 
     updater.start_webhook(
         listen='0.0.0.0',
